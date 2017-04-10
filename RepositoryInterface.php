9b78d810e446e66220a397cdbde3362463ef1f05
Gary <liaogling@gmail.com>
2017-04-10 11:31:22 +0800

    Add RepositoryInterface

---
+++ RepositoryInterface.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Liao Gengling <liaogling@gmail.com>
+ */
+
+namespace Planfox\Component\Config;
+
+interface RepositoryInterface
+{
+    /**
+     * set directory of configuration
+     *
+     * @param string $directory Absolute path
+     * @return mixed
+     */
+    public function setDirectory($directory);
+
+    /**
+     * Get directory of configuration
+     *
+     * @return mixed
+     */
+    public function getDirectory();
+
+    /**
+     *
+     * Get config value
+     *
+     * @param null|string $key
+     * @return mixed
+     */
+    public function get($key = null);
+
+    /**
+     * Set config value
+     *
+     * @param string $key
+     * @param mixed $vars
+     * @return mixed
+     */
+    public function set($key, $vars);
+}
